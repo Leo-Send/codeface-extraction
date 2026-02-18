9e6ccceb3caa91966ef0a9f56a696a9fe7a609e6
Leo Sendelbach <s8lesend@stud.uni-saarland.de>
2026-02-18 14:06:28 +0100

    Unify copilot users in all files
    unification now done on all files, which should prevent any issues
    arising from unknown authors during anonymization
    also move all global variables to a new utils file
    
    Signed-off-by: Leo Sendelbach <s8lesend@stud.uni-saarland.de>

--- issue_processing/issue_processing.py
+++ issue_processing/issue_processing.py
@@ -41,6 +41,7 @@ from codeface.dbmanager import DBManager
 from dateutil import parser as dateparser
 
 from csv_writer import csv_writer
+from github_user_utils import copilot_unified_name
 
 # known types from JIRA and GitHub default labels
 known_types = {"bug", "improvement", "enhancement", "feature", "task", "test", "wish"}
@@ -54,8 +55,6 @@ known_resolutions = {"unresolved", "fixed", "wontfix", "duplicate", "invalid", "
 # datetime format string
 datetime_format = "%Y-%m-%d %H:%M:%S"
 
-# Copilot username to be assigned in specific copilot events
-copilot_username = "Copilot"
 
 def run():
     # get all needed paths and arguments for the method call.
@@ -494,7 +493,7 @@ def merge_issue_events(issue_data, external_connected_events):
             # if event is a specific copilot event, assign the copilot user data
             if event["event"] == "copilot_work_started" or event["event"] == "copilot_work_finished":
                 event["user"]["name"] = None
-                event["user"]["username"] = copilot_username
+                event["user"]["username"] = copilot_unified_name
                 event["user"]["email"] = ""
 
             # if event dismisses a review, we can determine the original state of the corresponding review
